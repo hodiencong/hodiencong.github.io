4bd58455374ed2edf351b23990cafdba8257e9d0
CongHD <conghdql4@gmail.com>
2020-06-10 12:53:03 +0900

    remove php process language

--- js/changeLanguage.js
+++ js/changeLanguage.js
@@ -1,14 +1,39 @@
 function changeLanguage(language){
     
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-            var resData = JSON.parse(this.responseText);
-            setLanguage(resData);
-        }
-    };
-    xmlhttp.open("GET",  "././php/switch-language.php?language="+language, true);
-    xmlhttp.send();
+    // var xmlhttp = new XMLHttpRequest();
+    // xmlhttp.onreadystatechange = function() {
+    // if (this.readyState == 4 && this.status == 200) {
+    //         var resData = JSON.parse(this.responseText);
+    //         setLanguage(resData);
+    //     }
+    // };
+    // xmlhttp.open("GET",  "././php/switch-language.php?language="+language, true);
+    // xmlhttp.send();
+
+    $(document).ready(function() {
+        $.ajax({
+            type: "GET",
+            url: "language.csv",
+            dataType: "text",
+            success: function(data) {processData(data,language);}
+         });
+    });
+    
+}
+
+function processData(allText, language) {
+
+    var allTextLines = allText.split(/\r\n|\n/);
+
+    for(var i = 0;i < allTextLines.length;i++){
+        let entities = allTextLines[i];
+        let rowData = entities.split(",");
+
+        let tagId = rowData[0];
+        let tagValue = rowData[language];
+        $(`#${tagId}`).html(tagValue);
+    }
+    
 }
 
 function setLanguage(data){
@@ -19,4 +44,4 @@ function setLanguage(data){
     }
 }
 
-changeLanguage("en")
+changeLanguage(2)
